0ea700711bb2ef79be19487fd135f3c5f0447b84
Cru1seControl <57685626+Cru1seControl@users.noreply.github.com>
2019-11-13 10:59:18 -0500

    Update api.py
    Added json file reading function "apifileParse"

--- api.py
+++ api.py
@@ -21,3 +21,21 @@ def apiParse(url, arg=None, keys=False):
 
     except Exception as error:
         print(error)
+
+def apifileParse(filename, arg=None, keys=False):
+    try:
+        with open(filename, "r") as filereader:
+            json_data = json.loads(filereader.read())
+            if keys == False:
+                pass
+            else:
+                print("Keys:", json_data.keys())
+            
+            if not arg:
+                pprint.pprint(json_data)
+            else:
+                pprint.pprint(json_data[arg])
+            filereader.close()
+    
+    except Exception as fileerror:
+        print(fileerror)
